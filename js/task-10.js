f8a3836ff3439c03b25292c1372b55cf1f56f128
Daemon-L <lit.dmitriy.ua@gmail.com>
2021-10-17 22:33:23 +0300

    + task-08

--- js/task-10.js
+++ js/task-10.js
@@ -1,1 +1,9 @@
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+// }
 
+// const controls = document.querySelector('#controls');
+// const input = document.querySelector('input');
+// const createBtn = document.querySelector('button[data-create]');
+// const destroyBtn = document.querySelector('button[data-destroy]');
+// const divBoxes = document.querySelector('#boxes');
